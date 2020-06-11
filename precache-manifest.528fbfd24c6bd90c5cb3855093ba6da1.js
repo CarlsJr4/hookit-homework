6459047dd896751dc150535d280cfb30cebf1a79
Carl Dungca <carl.dungca@yahoo.com>
2020-06-11 13:43:17 -0700

    Updates

---
+++ precache-manifest.528fbfd24c6bd90c5cb3855093ba6da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6f2bd0d75af8ee0ab1f8291d516236",
+    "url": "/hookit-homework/index.html"
+  },
+  {
+    "revision": "6b79c520ef8a71b1ce1c",
+    "url": "/hookit-homework/static/css/main.0148b045.chunk.css"
+  },
+  {
+    "revision": "2c923989a211fae67c56",
+    "url": "/hookit-homework/static/js/2.30ff1b32.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/hookit-homework/static/js/2.30ff1b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b79c520ef8a71b1ce1c",
+    "url": "/hookit-homework/static/js/main.1b3ad032.chunk.js"
+  },
+  {
+    "revision": "3ac5702ddf841e3c2743",
+    "url": "/hookit-homework/static/js/runtime-main.39690c17.js"
+  }
+]);
